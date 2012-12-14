eb6849f46a1f71a4aaa5269dd095154b924d0c29
Leander Hutton <leander@obafgkm.local>
2012-12-13 19:21:41 -0500

    Added intial script and updated README

---
+++ README.txt
@@ -0,0 +1,14 @@
+Google Calendar Backup Script
+Saves your Google Calendars in ICS format from private or public links that can then be imported into most calendaring applications.
+This script really isn't meant for syncing, just backup in case Google's cloud explodes and loses your data.
+
+Version 1.0 
+Leander Hutton
+www.one-button.org
+github.com/lhutton
+
+
+Requires:
+Python 
+urllib2
+datetime
---
+++ gcal_backup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import sys
+import os
+import urllib2
+import datetime
+
+# Google Calendar Backup Script
+# Saves your Google Calendars in ICS format from private or public links
+# that can then be imported into most calendaring applications.
+# This script really isn't meant for syncing, just backup in case Google's
+# cloud explodes and loses your data.
+
+# Version 1.0 
+# Leander Hutton
+# www.one-button.org
+# github.com/lhutton
+
+# Directroy where your backups are stored
+backupdir = "/home/username/gcalbackups/"
+# Key values store of your calendar names and private URLs
+calendars = {'CalenderName1': 'https://CalendarPrivateURL1', 'CalendarName2': 'https://CalenderPrivateURL2'}
+
+# Get the current date 
+currentdate = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M')
+
+try:
+	# test to see if the backup directory exists
+	# if not create it
+	if not os.path.exists(backupdir):
+		os.makedirs(backupdir)
+	# download the files
+	# write them to disk
+	keys = calendars.keys() 
+	for calendar in keys:	
+		response = urllib2.urlopen(calendars[calendar])
+		cal = response.read()
+		os.chdir(backupdir)
+		filename = calendar + ' ' + currentdate + '.ics'
+		calfile = open(filename, "w")
+		calfile.write(cal)
+		calfile.close()
+except IOError, e:
+	print "I/O error({0}): {1})".format(e.errno, e.sterror)
+except:
+	print "Unknown error:", sys.exc_info()[0]
+	raise
