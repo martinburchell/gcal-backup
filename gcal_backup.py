4d6c99fb18c806ed8a92822d8a7a4f2ac0f43ae7
Martin Burchell <martinburchell@spamcop.net>
2014-06-22 20:12:37 +0100

    PEP8 fixes

--- gcal_backup.py
+++ gcal_backup.py
@@ -10,35 +10,34 @@ import datetime
 # This script really isn't meant for syncing, just backup in case Google's
 # cloud explodes and loses your data.
 
-# Version 1.0 
+# Version 1.0
 # Leander Hutton
 # www.one-button.org
 # github.com/lhutton
 
-from settings import *
+from settings import backupdir, calendars
 
-
-# Get the current date 
+# Get the current date
 currentdate = datetime.datetime.now().strftime('%Y-%m-%d')
 
 try:
-	# test to see if the backup directory exists
-	# if not create it
-	if not os.path.exists(backupdir):
-		os.makedirs(backupdir)
-	# download the files
-	# write them to disk
-	keys = calendars.keys() 
-	for calendar in keys:	
-		response = urllib2.urlopen(calendars[calendar])
-		cal = response.read()
-		os.chdir(backupdir)
-		filename = calendar + '-' + currentdate + '.ics'
-		calfile = open(filename, "w")
-		calfile.write(cal)
-		calfile.close()
+    # test to see if the backup directory exists
+    # if not create it
+    if not os.path.exists(backupdir):
+        os.makedirs(backupdir)
+    # download the files
+    # write them to disk
+    keys = calendars.keys()
+    for calendar in keys:
+        response = urllib2.urlopen(calendars[calendar])
+        cal = response.read()
+        os.chdir(backupdir)
+        filename = calendar + '-' + currentdate + '.ics'
+        calfile = open(filename, "w")
+        calfile.write(cal)
+        calfile.close()
 except IOError, e:
-	print "I/O error({0}): {1})".format(e.errno, e.sterror)
+    print "I/O error({0}): {1})".format(e.errno, e.sterror)
 except:
-	print "Unknown error:", sys.exc_info()[0]
-	raise
+    print "Unknown error:", sys.exc_info()[0]
+    raise
